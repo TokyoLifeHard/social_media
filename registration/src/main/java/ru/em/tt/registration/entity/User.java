d20c7e38dbae44fafabfc28b2a48246cbb9d771d
Kawaiiskii <antonzhadnov@gmail.com>
2023-08-17 00:29:02 +0300

    add registration service

---
+++ registration/src/main/java/ru/em/tt/registration/entity/User.java
@@ -0,0 +1,10 @@
+package ru.em.tt.registration.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_user")
+public class User {
+
+}
---
+++ registration/src/test/java/ru/em/tt/registration/RegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.em.tt.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
