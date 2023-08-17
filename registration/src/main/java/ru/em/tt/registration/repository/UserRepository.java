ee0ad645e6682ea0f62200aaf8e0f4846ffa510d
Kawaiiskii <antonzhadnov@gmail.com>
2023-08-17 20:32:00 +0300

    implement UserRepository

--- registration/src/main/java/ru/em/tt/registration/repository/UserRepository.java
+++ registration/src/main/java/ru/em/tt/registration/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package ru.em.tt.registration.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.em.tt.registration.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
 }
