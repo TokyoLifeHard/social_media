59e6aa836f75b7c9037d418996fbb58e89b1ae76
Kawaiiskii <antonzhadnov@gmail.com>
2023-08-17 20:41:30 +0300

    implement UserService

--- registration/src/main/java/ru/em/tt/registration/service/UserService.java
+++ registration/src/main/java/ru/em/tt/registration/service/UserService.java
@@ -1,4 +1,20 @@
 package ru.em.tt.registration.service;
 
+import org.springframework.stereotype.Service;
+import ru.em.tt.registration.entity.User;
+import ru.em.tt.registration.repository.UserRepository;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        User savedUser = this.userRepository.save(user);
+        return savedUser;
+    }
 }
