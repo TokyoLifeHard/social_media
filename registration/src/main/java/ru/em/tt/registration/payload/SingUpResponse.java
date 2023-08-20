aa35ac04babc564a68aee19bcc4a0a80719874ec
Kawaiiskii <antonzhadnov@gmail.com>
2023-08-20 18:37:16 +0300

    add SingUpResponse

--- registration/src/main/java/ru/em/tt/registration/payload/SingUpResponse.java
+++ registration/src/main/java/ru/em/tt/registration/payload/SingUpResponse.java
@@ -1,4 +1,27 @@
 package ru.em.tt.registration.payload;
 
 public class SingUpResponse {
+    private String message;
+    private int status;
+
+    public SingUpResponse(String message, int status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
