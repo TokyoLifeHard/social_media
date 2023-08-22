5fc19cf470788ec3f23e7acf2296ad3e7bcd83c3
Kawaiiskii <antonzhadnov@gmail.com>
2023-08-22 23:40:52 +0300

    implement UserRepository in authorization service

--- authorization/src/main/java/ru/em/tt/authorization/repository/UserRepository.java
+++ authorization/src/main/java/ru/em/tt/authorization/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package ru.em.tt.authorization.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import ru.em.tt.authorization.entity.User;
+
+import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User,Long> {
+    Optional<User> findByUsername(String username);
+
 }
